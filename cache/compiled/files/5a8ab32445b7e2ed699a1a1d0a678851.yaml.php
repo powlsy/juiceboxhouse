a9872834a4b0f2b6141e99c8de56292da33452a6
powlsy <git@powlsy.org>
2017-05-27 22:00:41 -0400

    remove ignored files

--- cache/compiled/files/5a8ab32445b7e2ed699a1a1d0a678851.yaml.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
-    'filename' => '/home/grav/www/html/user/config/security.yaml',
-    'modified' => 1495932471,
-    'data' => [
-        'salt' => 'rcuNBdReFL76vj'
-    ]
-];
